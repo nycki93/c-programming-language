18fad057010687d1ab69fbb4efdfc21d3524849e
Nick Lamicela <nupanick@gmail.com>
2021-12-31 12:08:49 -0800

    Refactor to wrap coroutine business

--- child_process_windows.c
+++ child_process_windows.c
@@ -3,73 +3,87 @@
 
 #define BUFFER_SIZE 1024
 
-int main(int num_args, char** args)
-{
-    /* Temporary flag for storing pass/fail status. */
+typedef struct MY_COROUTINE {
+    HANDLE in;
+    HANDLE out;
+} MY_COROUTINE;
+
+BOOL spawn(char* cmd, MY_COROUTINE* out_cr) {
+    // Temp flag
     BOOL t = TRUE;
 
-    /* Set up input and output pipes. */
-
+    // Inherit all handles except for write-to-in and read-from-out.
     SECURITY_ATTRIBUTES sa = {0};
-    sa.nLength = sizeof(SECURITY_ATTRIBUTES);
+    sa.nLength = sizeof(sa);
     sa.bInheritHandle = TRUE;
 
-    HANDLE child_stdout_read;
-    HANDLE child_stdout_write;
-    t = t && CreatePipe(&child_stdout_read, &child_stdout_write, &sa, 0);
-    t = t && SetHandleInformation(child_stdout_read, HANDLE_FLAG_INHERIT, 0);
+    HANDLE in_read;
+    HANDLE in_write;
+    t = t && CreatePipe(&in_read, &in_write, &sa, 0);
+    t = t && SetHandleInformation(in_write, HANDLE_FLAG_INHERIT, 0);
 
-    HANDLE child_stdin_read;
-    HANDLE child_stdin_write;
-    t = t && CreatePipe(&child_stdin_read, &child_stdin_write, &sa, 0);
-    t = t && SetHandleInformation(child_stdin_write, HANDLE_FLAG_INHERIT, 0);
+    HANDLE out_read;
+    HANDLE out_write;
+    t = t && CreatePipe(&out_read, &out_write, &sa, 0);
+    t = t && SetHandleInformation(out_read, HANDLE_FLAG_INHERIT, 0);
 
-    if (!t) {
-        printf("Failed to create pipes.\n");
-        return 1;
-    }
-
-    /* Start child process. */
-    
-    STARTUPINFO si = {0};
-    si.cb = sizeof(si);
-    si.hStdOutput = child_stdout_write;
-    si.hStdInput = child_stdin_read;
-    si.dwFlags = STARTF_USESTDHANDLES;
+    // Failed to open pipes?
+    if (!t) return FALSE; 
 
     PROCESS_INFORMATION pi = {0};
-    
-    char* cmd = "cmd /c echo hello world";
-    
-    t = CreateProcess(NULL, cmd, NULL, NULL, TRUE, 0, NULL, NULL, &si, &pi);
+    STARTUPINFO si = {0};
+    si.cb = sizeof(si);
+    si.hStdOutput = out_write;
+    si.hStdInput = in_read;
+    si.dwFlags = STARTF_USESTDHANDLES;
+    t = CreateProcess(0, cmd, 0, 0, 1, 0, 0, 0, &si, &pi);
+    // Failed to start process?
+    if (!t) return FALSE; 
+
+    // It is possible that the process fails after starting.
+    // Future improvement: can we capture that info?
+
+    // Close all handles that we don't return.
+    CloseHandle(pi.hProcess);
+    CloseHandle(pi.hThread);
+    CloseHandle(out_write);
+    CloseHandle(in_read);
+
+    out_cr->in = in_write;
+    out_cr->out = out_read;
+
+    return TRUE;
+}
+
+BOOL close(MY_COROUTINE* cr) {
+    CloseHandle(cr->in);
+    CloseHandle(cr->out);
+    return TRUE;
+}
+
+int main(int num_args, char **args) 
+{
+    BOOL t;
+
+    char *cmd = "cmd /c echo hello world";
+    MY_COROUTINE cr = {0};
+    t = spawn(cmd, &cr);
     if (!t) {
-        printf("Failed to start process.\n");
+        printf("Failed to spawn coroutine.");
         return 1;
     }
 
-    /* Close unnecessary handles or else the child might not exit. */
-
-    CloseHandle(pi.hProcess);
-    CloseHandle(pi.hThread);
-    CloseHandle(child_stdout_write);
-    CloseHandle(child_stdin_read);
-
     BYTE buffer[BUFFER_SIZE];
-    DWORD num_read;
-    DWORD num_written;
-    HANDLE parent_stdout = GetStdHandle(STD_OUTPUT_HANDLE);
+    DWORD num_read, num_written;
+    HANDLE my_out = GetStdHandle(STD_OUTPUT_HANDLE);
     printf("<child process> ");
     while (1) {
-        t = ReadFile(child_stdout_read, buffer, BUFFER_SIZE, &num_read, NULL);
+        t = ReadFile(cr.out, buffer, BUFFER_SIZE, &num_read, NULL);
         if (!t || num_read == 0) break;    
-        t = WriteFile(parent_stdout, buffer, num_read, &num_written, NULL);
+        t = WriteFile(my_out, buffer, num_read, &num_written, NULL);
         if (!t) break;
     }
 
-    /*  Close remaining handles to be neat and tidy. */
-
-    CloseHandle(child_stdout_read);
-    CloseHandle(child_stdin_write);
-
-    return 0;    
+    close(&cr);
+    return 0;
 }
