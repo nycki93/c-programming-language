667a0f1ff120af7899b92159021428da959737d9
Nicholas Lamicela <nupanick@gmail.com>
2021-12-30 22:23:00 -0800

    Child process example working on windows!

--- child_process_windows.c
+++ child_process_windows.c
@@ -3,21 +3,29 @@
 
 int main(int num_args, char** args)
 {
+    BOOL t = TRUE;
+
     /* Set up pipes for child's input and output. */
 
     SECURITY_ATTRIBUTES sa = {0};
-    sa.nLength = sizeof(sa);
+    sa.nLength = sizeof(SECURITY_ATTRIBUTES);
     sa.bInheritHandle = TRUE;
+    sa.lpSecurityDescriptor = NULL;
 
-    HANDLE child_stdin_read;
-    HANDLE child_stdin_write;
-    CreatePipe(&child_stdin_read, &child_stdin_write, &sa, 0);
-    SetHandleInformation(&child_stdin_write, HANDLE_FLAG_INHERIT, 0);
+    HANDLE child_stdout_read = NULL;
+    HANDLE child_stdout_write = NULL;
+    t = t && CreatePipe(&child_stdout_read, &child_stdout_write, &sa, 0);
+    t = t && SetHandleInformation(child_stdout_read, HANDLE_FLAG_INHERIT, 0);
 
-    HANDLE child_stdout_read;
-    HANDLE child_stdout_write;
-    CreatePipe(&child_stdout_read, &child_stdout_write, &sa, 0);
-    SetHandleInformation(child_stdout_read, HANDLE_FLAG_INHERIT, 0);
+    HANDLE child_stdin_read = NULL;
+    HANDLE child_stdin_write = NULL;
+    t = t && CreatePipe(&child_stdin_read, &child_stdin_write, &sa, 0);
+    t = t && SetHandleInformation(child_stdin_write, HANDLE_FLAG_INHERIT, 0);
+
+    if (!t) {
+        printf("Failed to create pipes.\n");
+        return 1;
+    }
 
     /* Start child process. */
     
@@ -29,33 +37,40 @@ int main(int num_args, char** args)
 
     PROCESS_INFORMATION pi = {0};
     
-    char* cmd = "hello";
+    char* cmd = "cmd /c echo hello world";
     
-    printf("Starting child process...\n");
-    CreateProcess(0, cmd, 0, 0, 0, 0, 0, 0, &si, &pi);
+    t = CreateProcess(NULL, cmd, NULL, NULL, TRUE, 0, NULL, NULL, &si, &pi);
+    if (!t) {
+        printf("Failed to start process.\n");
+        return 1;
+    }
 
-    /* Read child's output. */
-    CHAR buffer[1024];
+    // These handles would be used to monitor the child process, but
+    // we're not using them.
+    CloseHandle(pi.hProcess);
+    CloseHandle(pi.hThread);
+
+    // These handles need to be explicitly closed or else the child will
+    // run indefinitely.
+    CloseHandle(child_stdout_write);
+    CloseHandle(child_stdin_read);
+
+    BYTE buffer[1024];
     DWORD num_read;
     DWORD num_written;
-    BOOL t = FALSE;
     HANDLE parent_stdout = GetStdHandle(STD_OUTPUT_HANDLE);
-    printf("Child process's output:\n");
+    printf("<child process> ");
     while (1) {
-        printf("About to read from child pipe.\n");
         t = ReadFile(child_stdout_read, buffer, 1024, &num_read, NULL);
-        if (!t || num_read == 0) {
-            printf("Failed to read from child pipe.\n");
-            break;
-        }
-
+        if (!t || num_read == 0) break;
+        
         t = WriteFile(parent_stdout, buffer, num_read, &num_written, NULL);
         if (!t) break;
     }
 
-    WaitForSingleObject(pi.hProcess, INFINITE);
+    // Close remaining handles to be neat and tidy.
+    CloseHandle(child_stdout_read);
+    CloseHandle(child_stdin_write);
 
-    CloseHandle(pi.hProcess);
-    CloseHandle(pi.hThread);
-    return 0;
+    return 0;    
 }
