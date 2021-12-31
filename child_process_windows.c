3b7e0a8c94d50d8e6069024686d30d0a6097cf2e
Nicholas Lamicela <nupanick@gmail.com>
2021-12-30 23:39:59 -0800

    organization

--- child_process_windows.c
+++ child_process_windows.c
@@ -1,24 +1,26 @@
 #include <stdio.h>
 #include <windows.h>
 
+#define BUFFER_SIZE 1024
+
 int main(int num_args, char** args)
 {
+    /* Temporary flag for storing pass/fail status. */
     BOOL t = TRUE;
 
-    /* Set up pipes for child's input and output. */
+    /* Set up input and output pipes. */
 
     SECURITY_ATTRIBUTES sa = {0};
     sa.nLength = sizeof(SECURITY_ATTRIBUTES);
     sa.bInheritHandle = TRUE;
-    sa.lpSecurityDescriptor = NULL;
 
-    HANDLE child_stdout_read = NULL;
-    HANDLE child_stdout_write = NULL;
+    HANDLE child_stdout_read;
+    HANDLE child_stdout_write;
     t = t && CreatePipe(&child_stdout_read, &child_stdout_write, &sa, 0);
     t = t && SetHandleInformation(child_stdout_read, HANDLE_FLAG_INHERIT, 0);
 
-    HANDLE child_stdin_read = NULL;
-    HANDLE child_stdin_write = NULL;
+    HANDLE child_stdin_read;
+    HANDLE child_stdin_write;
     t = t && CreatePipe(&child_stdin_read, &child_stdin_write, &sa, 0);
     t = t && SetHandleInformation(child_stdin_write, HANDLE_FLAG_INHERIT, 0);
 
@@ -45,30 +47,27 @@ int main(int num_args, char** args)
         return 1;
     }
 
-    // These handles would be used to monitor the child process, but
-    // we're not using them.
+    /* Close unnecessary handles or else the child might not exit. */
+
     CloseHandle(pi.hProcess);
     CloseHandle(pi.hThread);
-
-    // These handles need to be explicitly closed or else the child will
-    // run indefinitely.
     CloseHandle(child_stdout_write);
     CloseHandle(child_stdin_read);
 
-    BYTE buffer[1024];
+    BYTE buffer[BUFFER_SIZE];
     DWORD num_read;
     DWORD num_written;
     HANDLE parent_stdout = GetStdHandle(STD_OUTPUT_HANDLE);
     printf("<child process> ");
     while (1) {
-        t = ReadFile(child_stdout_read, buffer, 1024, &num_read, NULL);
-        if (!t || num_read == 0) break;
-        
+        t = ReadFile(child_stdout_read, buffer, BUFFER_SIZE, &num_read, NULL);
+        if (!t || num_read == 0) break;    
         t = WriteFile(parent_stdout, buffer, num_read, &num_written, NULL);
         if (!t) break;
     }
 
-    // Close remaining handles to be neat and tidy.
+    /*  Close remaining handles to be neat and tidy. */
+
     CloseHandle(child_stdout_read);
     CloseHandle(child_stdin_write);
 
